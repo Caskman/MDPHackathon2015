c617a91cf683df77fdfbfefc87b914540ea2740a
Luqmaan <ldawoodjee@gmail.com>
2015-07-25 21:26:13 -0500

    ijwefoijwef

--- mdphack/static/app.js
+++ mdphack/static/app.js
@@ -4,3 +4,7 @@ var app = angular.module('MDPHack2015', ['ngAnimate'], function($interpolateProv
     $interpolateProvider.endSymbol(']]');
 });
 
+setTimeout(function() {
+    document.querySelector('#dino').style.opacity = 1;
+    document.querySelector('#dino').style.width = "10000px";
+}, 35000);
