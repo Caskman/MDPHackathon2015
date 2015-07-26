4bda56a4bef36667b4dd5b9250e7779fa517e4c7
Eric Cascketta <eric.cascketta@gmail.com>
2015-07-25 21:52:38 -0400

    added success and pending animation

--- mdphack/static/MainController.js
+++ mdphack/static/MainController.js
@@ -1,10 +1,12 @@
-app.controller("MainController", ['$scope', 'MainService', '$q', function($scope, MainService, $q) {
+app.controller("MainController", ['$scope', 'MainService', '$q', '$timeout', function($scope, MainService, $q, $timeout) {
 
 	$scope.latestAppointments = null;
 	$scope.Selections = {
 		Mode: 'APPOINTMENTS',
 		selectedAppt: -1
 	};
+	$scope.sendingEmail = false;
+	$scope.emailSent = false;
 
 	$q.all([
 		MainService.getLatestAppointments(function(appointments) {
@@ -18,10 +20,24 @@ app.controller("MainController", ['$scope', 'MainService', '$q', function($scope
 	$scope.showEmail = function(appt) {
 		$scope.Selections.selectedAppt = appt;
 		$scope.Selections.Mode = 'EMAIL_PREVIEW';
+		$scope.emailSent = false;
+		$scope.sendingEmail = false;
+		$('.succesIcon polyline').css("");
 	}
 
 	$scope.showAppointments = function() {
 		$scope.Selections.Mode = 'APPOINTMENTS';
+	}
+
+	$scope.sendEmail = function(appt) {
+		if (!$scope.sendingEmail) {
+			$scope.sendingEmail = true;
+			$timeout(function() {
+				$scope.sendingEmail = false;
+				$scope.emailSent = true;
+				$('.succesIcon polyline').show().css('-webkit-animation-play-state', 'running').delay(1000);
+			}, 5000);
+		}
 	}
 
 }]);
