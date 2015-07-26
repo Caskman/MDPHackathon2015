c9271c7bd369b5031c34cfb1e80ab08fee1c4a33
Eric Cascketta <eric.cascketta@gmail.com>
2015-07-25 22:21:50 -0400

    more stuff

--- mdphack/static/app.js
+++ mdphack/static/app.js
@@ -1,5 +1,5 @@
 // var app = angular.module('MDPHack2015',[]);
-var app = angular.module('MDPHack2015', [], function($interpolateProvider) {
+var app = angular.module('MDPHack2015', ['ngAnimate'], function($interpolateProvider) {
     $interpolateProvider.startSymbol('[[');
     $interpolateProvider.endSymbol(']]');
 });
